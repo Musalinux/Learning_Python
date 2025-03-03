987484bf1c2fa6e03256e1f7accc3f33794e2ac3
Musalinux <vasaikarmusaddik@gmail.com>
2025-03-03 01:57:09 +0000

    Daily Update

---
+++ Python_problems_for_DevOps/6_Reverse-a-string.py
@@ -0,0 +1,10 @@
+def str_rev (Mystring):
+    reverseString = ""
+    for i in MyString:
+        reverseString = i + reverseString
+    return reverseString
+
+MyString = input("Enter a string and I will reverse it for you: \n")
+
+reversed_string = str_rev(MyString)
+print (f"Reverse of {MyString} is '{reversed_string}' ")
