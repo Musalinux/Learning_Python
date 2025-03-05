b8dece18fb2bb62edb03d1640361f6f86d1a2bbf
Musalinux <vasaikarmusaddik@gmail.com>
2025-03-05 02:51:15 +0000

    Daily Update

---
+++ Python_problems_for_DevOps/12_prime-or-not.py
@@ -0,0 +1,23 @@
+def primeOrNot (number):
+    if number == 1:
+      return "not a prime number"
+    for i in range (2, number):
+        if (number % i ==0):
+            return "not a prime number"
+    else: 
+        return "a prime number"
+        
+user_input = ""
+while True:
+  user_input = input ("Enter a number, I will tell you if it is a prime number: \n")
+  if user_input.lower() == "stop":
+    print ("End of program")
+    break 
+  try:
+    number  = int(user_input)
+
+    if number >  0:
+        result = primeOrNot(number)
+        print (f"{number} is {result}")
+  except ValueError:
+    print ("Invalid input! Please enter a number")
