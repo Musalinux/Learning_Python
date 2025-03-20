e66d2cfb322f290be0783cacbc0bc4c7d3e507e2
Musalinux <vasaikarmusaddik@gmail.com>
2025-03-20 22:53:33 +0000

    Daily Update

--- Python_problems_for_DevOps/strings.py
+++ Python_problems_for_DevOps/strings.py
@@ -12,14 +12,17 @@ The middle 3 characters of the string (if the string has an odd length).
 
 def printer(user_input):
     length = len (user_input)
-    if length > 5 and length % 2 ! = 0:
+    if length >= 5: 
         print(user_input)
         print(f"The first 5 characters are: {user_input[0:5]}")
         print(f"The last 5 characters are: {user_input[-5:]}")
-        char = len(user_input)
-        char = char // 2
-        mid3char = user_input[char - 1:char + 2]
-        print(f"The middle 3 characters are: {mid3char}")
+        if length % 2 != 0:
+            char = len(user_input)
+            char = char // 2
+            mid3char = user_input[char - 1:char + 2]
+            print(f"The middle 3 characters are: {mid3char}")
+        else: 
+            print ("No middle 3 characters, since the string is even length")
     else:
         print("please enter a string with length odd and more than 5")
 
