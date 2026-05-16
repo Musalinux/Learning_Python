66963751d8ee3bad7c79d4ac5eebf06880a97f62
Musaddik Vasaikar <vasaikarmusaddik@gmail.com>
2026-05-16 19:58:33 +0100

    learning devops problems

--- DevOps Problems/01_hello.py
+++ DevOps Problems/01_hello.py
@@ -5,3 +5,15 @@ print(result1)
 
 result2 = arn.upper()
 print (result2)
+
+result3 = len (arn)
+print (result3)
+
+name = "My name is John"
+
+replaceName = name.replace ("John", "Musa")
+print (replaceName)
+
+text = "   Some spaces around   "
+stripped_text = text.strip()
+print("Stripped text:", stripped_text)
