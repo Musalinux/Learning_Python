d4f3d87ad33a4f4a526f3b834f089ace24522da1
Musaddik Vasaikar <vasaikarmusaddik@gmail.com>
2026-05-17 14:51:01 +0100

    update

---
+++ DevOps Problems/05_list-and_tuple.py
@@ -0,0 +1,10 @@
+s3_bucket_list = ["123-bucker", "456-bucker", "789-bucker", "abc-bucker"]
+
+print (s3_bucket_list)
+
+print (len((s3_bucket_list)))
+
+s3_bucket_list.append("new-bucket")
+
+print (s3_bucket_list)
+print (len((s3_bucket_list)))
