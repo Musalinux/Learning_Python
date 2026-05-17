cc72bd076717d7fb4adce63a9818121c36ce95f8
Musaddik Vasaikar <vasaikarmusaddik@gmail.com>
2026-05-17 20:31:43 +0100

    update

---
+++ DevOps Problems/07_dict-ec2.py
@@ -0,0 +1,16 @@
+ec2_instance_info = [
+    {
+        "id": "instance-001",
+        "type": "t2.micro"
+    },
+    {
+        "id": "instance-001",
+        "type": "t2.medium"
+    },
+    {
+        "id": "instance-001",
+        "type": "t2.xlarge"
+    }
+]
+
+print (ec2_instance_info[2]["type"])
