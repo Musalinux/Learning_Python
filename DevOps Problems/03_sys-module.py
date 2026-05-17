aae5ea0e5de3217a28b1dad9570d4a27b7eca724
Musaddik Vasaikar <vasaikarmusaddik@gmail.com>
2026-05-17 13:31:22 +0100

    update 03_sys-module.py

--- DevOps Problems/03_sys-module.py
+++ DevOps Problems/03_sys-module.py
@@ -4,6 +4,17 @@ def add (num1, num2):
     output = num1 + num2
     return output 
 
+def sub (num1, num2):
+    output = num1 - num2
+    return output 
+
+def mul (num1, num2):
+    output = num1 * num2
+    return output 
+
+def div (num1, num2):
+    output = num1 // num2
+    return output 
 
 num1 = int(sys.argv[1])
 operation = sys.argv[2]
@@ -11,4 +22,13 @@ num2 = int(sys.argv[3])
 
 if operation == "add":
     result = add (num1, num2)
+    print (result)
+elif operation == "sub":
+    result = sub (num1, num2)
+    print (result)
+elif operation == "mul":
+    result = mul (num1, num2)
+    print (result)
+elif operation == "div":
+    result = div (num1, num2)
     print (result)
