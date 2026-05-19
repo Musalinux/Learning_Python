9ef8aa96f1c4271d17174c51c0583a14bf4fcab1
Musaddik Vasaikar <vasaikarmusaddik@gmail.com>
2026-05-19 01:11:57 +0100

    update and add jira github script

---
+++ DevOps Problems/15_JIRA_create-ticket-using-flask.py
@@ -0,0 +1,50 @@
+# we just import flask module 
+from flask import Flask
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import os 
+
+# Create a flask app instance
+app = Flask(__name__)
+
+# add a decorator. This performs an action before execution of the function
+@app.route("/createJira", methods = ['POST'])
+
+# now define the function 
+def createJira ():
+    url = "https://vasaikarmusaddik.atlassian.net/rest/api/3/issue"
+
+    auth = HTTPBasicAuth("vasaikarmusaddik@gmail.com", os.getenv("JIRA_API_TOKEN"))
+
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    payload = json.dumps( {
+      "fields": {
+        "issuetype": {
+          "id": "10003"
+        },
+        "project": {
+          "key": "SCRUM"
+        },
+        "reporter": {
+          "id": "61b613280f02490069c7bb05"
+        },
+        "summary": "Musa second Jira ticket"
+      },
+      "update": {}
+    } )
+    response = requests.request(
+        "POST",
+        url,
+        data=payload,
+        headers=headers,
+        auth=auth
+    )
+
+    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+# Flask comes with an inbuilt server, so you dont need to deploy it anywhere
+
+app.run ('0.0.0.0', port=9000)
